7459092e280006e50727c154171b44fa9b09858e
bhumikapatel251 <102209491+bhumikapatel251@users.noreply.github.com>
2022-07-05 15:22:27 +0530

    Initial Commit

---
+++ PizzaApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PizzaApp
+//
+//  Created by Bhumika Patel on 05/07/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PizzaApp/PizzaAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PizzaAppApp.swift
+//  PizzaApp
+//
+//  Created by Bhumika Patel on 05/07/22.
+//
+
+import SwiftUI
+
+@main
+struct PizzaAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
