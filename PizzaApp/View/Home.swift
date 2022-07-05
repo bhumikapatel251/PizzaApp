fe4ee65b669d0a63a5daee716292962019f1a6ce
bhumikapatel251 <102209491+bhumikapatel251@users.noreply.github.com>
2022-07-05 15:53:49 +0530

    set menu in system image and person image

--- PizzaApp/View/Home.swift
+++ PizzaApp/View/Home.swift
@@ -9,7 +9,28 @@ import SwiftUI
 
 struct Home: View {
     var body: some View {
-     
+        VStack{
+            HStack{
+                Button{
+                    
+                } label: {
+                    Image(systemName: "square.grid.2x2")
+                        .foregroundColor(.white)
+                        .frame(width: 20, height: 20)
+                }
+                Spacer()
+                Button{
+                    
+                } label: {
+                    Image(systemName: "person")
+                        .foregroundColor(.white)
+                        .frame(width: 38, height: 38)
+                        .clipShape(Circle())
+                }
+            }
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .top)
+        .padding(15)
     }
 }
 
