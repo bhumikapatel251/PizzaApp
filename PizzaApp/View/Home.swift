1f57d844a1ce200107257f394684177008a6fd1c
bhumikapatel251 <102209491+bhumikapatel251@users.noreply.github.com>
2022-07-05 16:40:56 +0530

    Pizza name and description display on screen

--- PizzaApp/View/Home.swift
+++ PizzaApp/View/Home.swift
@@ -8,6 +8,7 @@
 import SwiftUI
 
 struct Home: View {
+    @State var selectedPizza: Pizza = Pizza[0]
     var body: some View {
         VStack{
             HStack{
@@ -28,10 +29,48 @@ struct Home: View {
                         .clipShape(Circle())
                 }
             }
+            .overlay{
+                Text("EATPIZZA")
+                    .font(.title)
+            }
+            Text("Select Your Pizza".uppercased())
+                .font(.caption)
+                .foregroundColor(.gray)
+                .padding(.top,15)
+            AnimatedSlider()
         }
         .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .top)
         .padding(15)
     }
+    @ViewBuilder
+    func AnimatedSlider()->some View{
+        GeometryReader{proxy in
+            let size = proxy.size
+            LazyHStack(spacing: 0){
+                ForEach(Pizza){pizza in
+                    VStack(spacing: 0){
+                        Text(pizza.pizzaTitle)
+                            .font(.largeTitle.bold())
+                        Text(pizza.pizzaDescription)
+                            .font(.callout)
+                            .multilineTextAlignment(.center)
+                            .foregroundColor(.white.opacity(0.7))
+                            .padding(.horizontal)
+                            .padding(.top,10)
+                    }
+                    .frame(width: size.width, height: size.height, alignment: .top)
+                }
+            }
+            PizzaView()
+        }
+        .padding(.horizontal,-15)
+        .padding(.top,35)
+    }
+    @ViewBuilder
+    func PizzaView()->some View{
+        let 
+    }
+    
 }
 
 struct Home_Previews: PreviewProvider {
